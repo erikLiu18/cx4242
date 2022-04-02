34d04e4b71013d27611548ca9e874d74308dd8c5
Liu <liu@lawn-143-215-120-138.lawn.gatech.edu>
2022-04-02 14:17:13 -0400

    update with notebook

--- API/main.js
+++ API/main.js
@@ -30,16 +30,53 @@ async function getOwners() {
     console.log(nftOwners)
 }
 
-async function getTrades() {
-    const options = { address: "0xf61F24c2d93bF2dE187546B14425BF631F28d6dC", limit: "10", chain: "eth" };
+async function getTrades(collection_hash, limit=10) {
+    const options = { address: collection_hash, limit: "10", chain: "eth" };
     const NFTTrades = await Moralis.Web3API.token.getNFTTrades(options);
     console.log(NFTTrades)
 }
+
+async function getContractTransfers(options) {
+  // const options = {
+  //   address: collection_hash,
+  //   limit: "500",
+  //   // offset: "0",
+  //   chain: "eth",
+  // };
+  const nftTransfers = await Moralis.Web3API.token.getContractNFTTransfers(options);
+  console.log("------")
+  return nftTransfers
+}
+
+// async function getPages(collection_hash) {
+//   list = []
+//   var options = {
+//     address: collection_hash,
+//     limit: "500",
+//     // offset: "0",
+//     chain: "eth",
+//   };
+//   Moralis.Web3API.token.getContractNFTTransfers(options).then(response => {
+//     console.log(response)
+//     list = list.concat(response.result)
+//     options = {
+//       address: collection_hash,
+//       limit: "500",
+//       chain: "eth",
+//       cursor: response.cursor
+//     }
+//     const nftTransfers = Moralis.Web3API.token.getContractNFTTransfers(options);
+//     console.log(nftTransfers)
+//     console.log(list)
+//   })
+// }
 
 async function logOut() {
   await Moralis.User.logOut();
   console.log("logged out");
 }
+
+ // collection_address, 0, [], 0
 
 async function getTransfers(nft_address,iteration,listOfTransfers,page){
   var iteration = iteration + 1;
@@ -59,14 +96,10 @@ async function getTransfers(nft_address,iteration,listOfTransfers,page){
     else{
       returnList = listOfTransfers.concat(response.result)
       console.log(response.cursor)
-      setTimeout(() => {getTransfers(nft_address,iteration,returnList,response.cursor)},250);
+      setTimeout(() => {getTransfers(nft_address,iteration,returnList,response.cursor)},40);
     }
   })
 }
-
-
-
-
 
 document.getElementById("btn-login").onclick = login;
 document.getElementById("btn-logout").onclick = logOut;
